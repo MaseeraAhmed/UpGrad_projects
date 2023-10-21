2bfe1932164bbc44c1bc7efa7af624b0c439c690
Maseera Ahmed <madihaahmed2017@gmail.com>
2023-10-21 20:37:28 +0530

    --

--- API project/script.js
+++ API project/script.js
@@ -12,33 +12,6 @@ let bookTitle = document.getElementById("booktitle").value;
 
 }
 
-// let myRequest = new XMLHttpRequest();
-// myRequest.open(
-//     "GET",
-//     `https://www.googleapis.com/books/v1/volumes?q=${bookTitle}`
-// );
-
-//     myRequest.send();
-
-
-    // myRequest.onreadystatechange = () => {
-    //     if(myRequest.readyState===4 && myRequest.status===200) {
-    //         let convertedBooks = JSON.parse(myRequest.response)
-    //         let newBook = convertedBooks.items.map(
-    //             (book) =>
-    //             `<div class="book>
-    //             <img src=${book.volumeInfo.imageLinks.thumbnail}/>
-    //                 <h1>${book.volumeInfo.title}</h1>
-    //                 <p>${book.volumeInfo.description}</p>
-    //                 </div>`
-    //             )
-    //             .join("")
-    //             content +=newBook;
-
-    //     }
-    //     allBooks.innerHTML = content;
-    // };
-
     let response = await fetch(
         `https://www.googleapis.com/books/v1/volumes?q=${bookTitle}`
     );
