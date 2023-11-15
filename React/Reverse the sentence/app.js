6d94481a34bb122a6d02cce1fa2267c87dcc98e7
Maseera Ahmed <madihaahmed2017@gmail.com>
2023-11-15 22:58:15 +0530

    reverse the sentence app React

--- React/Reverse the sentence/app.js
+++ React/Reverse the sentence/app.js
@@ -19,7 +19,7 @@ class MyApp extends React.Component {
 
             <div className="top">
 
-            <input type="text" id="inputFld" onChange={e => this.setState({ txt: e.target.value })} />
+            <input type="text" id="inputFld"  onChange={e => this.setState({ txt: e.target.value })} />
 
             </div>
 
